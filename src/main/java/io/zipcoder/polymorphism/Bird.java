0a864f0454dae8e1f23ac7bfac8af15d6c34a8b7
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-11-12 13:24:09 -0500

    done with pet construction

--- src/main/java/io/zipcoder/polymorphism/Bird.java
+++ src/main/java/io/zipcoder/polymorphism/Bird.java
@@ -1,11 +1,22 @@
 package io.zipcoder.polymorphism;
 
 public class Bird extends Pet{
+private String birdsName;
 
-    Bird()
-    {
+    public void setBirdsName(String name) {
 
+        this.birdsName = birdsName;
     }
 
+    public String getBirdsName() {
 
+        return birdsName;
+    }
+
+    public Bird(String birdsName) {
+        this.birdsName = birdsName;
+    }
+    public String speak(){
+        return "tweet";
+    }
 }
--- src/main/java/io/zipcoder/polymorphism/Cat.java
+++ src/main/java/io/zipcoder/polymorphism/Cat.java
@@ -1,7 +1,15 @@
 package io.zipcoder.polymorphism;
 
 public class Cat extends Pet {
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+
     private String catName;
+
+    public String getCatName() {
+        return catName;
+    }
 
     Cat(String name)
     {
